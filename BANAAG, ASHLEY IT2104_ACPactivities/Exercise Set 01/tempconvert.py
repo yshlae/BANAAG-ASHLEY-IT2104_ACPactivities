911cc109583f7f4a7999ce184c5ed1ea933d85ef
yshlae <23-02091@g.batstate-u.edu.ph>
2024-08-21 19:42:38 +0800

    Add files via upload

---
+++ BANAAG, ASHLEY IT2104_ACPactivities/Exercise Set 01/tempconvert.py
@@ -0,0 +1,3 @@
+celsius = float(input("Enter a Celsius value: "))
+fahrenheit = (celsius * 9 / 5) + 32
+print(f"{celsius:.2f} Celsius is {fahrenheit:.2f} Fahrenheit")
---
+++ BANAAG, ASHLEY IT2104_ACPactivities/Exercise Set 01/welcome.py
@@ -0,0 +1,4 @@
+print("Welcome to CS 121: Advance Computer Programming!\n")
+name = input("Enter your name: ")
+print("Your name is: ", name)
+print("This will be fun! ", name)
